4dccb24555d3b223d89db18cd2c131422456042a
Zurab <gostohov@gmail.com>
2023-07-29 12:18:27 +0300

    Implementing event producers

--- src/main/java/com/polarbookshop/orderservice/order/domain/OrderService.java
+++ src/main/java/com/polarbookshop/orderservice/order/domain/OrderService.java
@@ -2,18 +2,26 @@ package com.polarbookshop.orderservice.order.domain;
 
 import com.polarbookshop.orderservice.book.Book;
 import com.polarbookshop.orderservice.book.BookClient;
+import com.polarbookshop.orderservice.order.event.OrderAcceptedMessage;
 import com.polarbookshop.orderservice.order.event.OrderDispatchedMessage;
-import reactor.core.publisher.Flux;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 public class OrderService {
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
+
     private final OrderRepository orderRepository;
     private final BookClient bookClient;
-    public OrderService(OrderRepository orderRepository, BookClient bookClient) {
+    private final StreamBridge streamBridge;
+    public OrderService(OrderRepository orderRepository, BookClient bookClient, StreamBridge streamBridge) {
         this.orderRepository = orderRepository;
         this.bookClient = bookClient;
+        this.streamBridge = streamBridge;
     }
     public Flux<Order> getAllOrders() {
         return orderRepository.findAll();
@@ -62,5 +70,17 @@ public class OrderService {
             existingOrder.lastModifiedDate(),
             existingOrder.version()
         );
+    }
+
+    private void publishOrderAcceptedEvent(Order order) {
+        if (!order.status().equals(OrderStatus.ACCEPTED)) {
+            return;
+        }
+        var orderAcceptedMessage = new OrderAcceptedMessage(order.id());
+        log.info("Sending order accepted event with id: {}", order.id());
+        var result = streamBridge.send("acceptOrder-out-0",
+            orderAcceptedMessage);
+        log.info("Result of sending data for order with id {}: {}",
+            order.id(), result);
     }
 }
